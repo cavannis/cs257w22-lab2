cbfcce8e5f79645f32f90d54b499e04e2a69ce00
harley <chappellh@sou.edu>
2022-01-15 15:22:37 -0800

    yes c++

---
+++ greeting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string usrname;
+    cout << "enter your name";
+    cin >> usrname;
+    cout << "Hi there! " << usrname;
+}
---
+++ yes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i;
+    int num1 = 1;
+    int num2 = 2;
+    for (i = 0; num1 < num2; i--) 
+        cout <<"y" << endl;
+    
+}
